e8415b3e9b3d1424cde37b94b98f9f70fa6235fd
Alireza Rezapour <alireza.rezapour.me@gmail.com>
2025-06-15 06:56:32 +0330

    pricing-service: remove unexpected exception type

--- pricing-service/src/common/exceptions/unexpected.ts
+++
@@ -1,9 +0,0 @@
-import { BaseException } from './base';
-
-export class UnexpectedException extends BaseException {
-  readonly cause?: Error;
-  constructor(message: string, cause?: Error) {
-    super(message);
-    this.cause = cause;
-  }
-}
--- pricing-service/src/pricing/application/usecases/pricing.ts
+++ pricing-service/src/pricing/application/usecases/pricing.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { BaseException } from 'src/common/exceptions/base';
-import { InfrastructureException } from 'src/common/exceptions/infrastructure';
-import { UnexpectedException } from 'src/common/exceptions/unexpected';
 import { UseCase } from 'src/lib/usecase';
 import { InventoryPort } from 'src/pricing/domain/ports/inventory';
 import { MarketDataPort } from 'src/pricing/domain/ports/market-data';
@@ -22,15 +19,10 @@ export class CalculatePriceUseCase implements UseCase<Input, Output> {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   async execute(input?: Input): Promise<Output> {
-    try {
-      const price = await this.marketData.get();
-      const inventory = await this.inventoryRepo.get();
+    const price = await this.marketData.get();
+    const inventory = await this.inventoryRepo.get();
 
-      const adjustedPrice = this.pricingRuleService.apply(inventory, price);
-      return { price: adjustedPrice.getValue() };
-    } catch (error) {
-      if (error instanceof BaseException) throw error;
-      throw new UnexpectedException('price calculation failed', error);
-    }
+    const adjustedPrice = this.pricingRuleService.apply(inventory, price);
+    return { price: adjustedPrice.getValue() };
   }
 }
